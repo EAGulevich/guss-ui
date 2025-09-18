9c7f12394cf8300f29fce2ae781449b02c3a9b75
Гулевич Екатерина <meteorgul@gmail.com>
2025-09-18 17:28:05 +0300

    add header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,25 @@
+import type { CSSProperties } from "react";
+import { Flex, Typography } from "antd";
+
 import { useUserStore } from "../store.ts";
+
+const headerContentStyles: CSSProperties = {
+  height: "100%",
+};
 
 export const Header = () => {
   const user = useUserStore((state) => state.user);
 
   return (
-    <div>
-      Header - {user?.username} - {user?.role}
-    </div>
+    <Flex
+      justify={"space-between"}
+      align={"center"}
+      style={headerContentStyles}
+    >
+      <Typography.Text> todo tabName</Typography.Text>
+      <Typography.Text>
+        {user?.username} - {user?.role}
+      </Typography.Text>
+    </Flex>
   );
 };
