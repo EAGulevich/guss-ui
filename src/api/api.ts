9288b6c2adc21d8f44651b10f43bd617f69c5ef8
Гулевич Екатерина <meteorgul@gmail.com>
2025-09-19 01:57:43 +0300

    add interceptors

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { ROUTES } from "../routes.ts";
+
 const API_BASE_URL = import.meta.env.PROD
   ? import.meta.env.VITE_API_URL
   : "/api";
@@ -11,3 +13,22 @@ export const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      window.location.href = ROUTES.LOGIN.to;
+    }
+    return Promise.reject(error);
+  },
+);
+
+/* errors
+WRONG_PASSWORD
+FORBIDDEN
+ROUND_NOT_FOUND
+ROUND_NOT_ACTIVE
+UNAUTHORIZED
+INVALID_TOKEN
+ */
