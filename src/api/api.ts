2f43c132255050a08e24b4223545b08580c61e7f
Гулевич Екатерина <meteorgul@gmail.com>
2025-09-19 01:17:12 +0300

    fix url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.PROD ? "/" : "/api";
+const API_BASE_URL = import.meta.env.PROD
+  ? import.meta.env.VITE_API_URL
+  : "/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
