1a9e66d43ca6a328d9ab7228b71070bd7515b94c
Гулевич Екатерина <meteorgul@gmail.com>
2025-09-18 14:55:10 +0300

    add const routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import LoginPage from "./pages/LoginPage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
 import RoundPage from "./pages/RoundPage.tsx";
 import RoundListPage from "./pages/RoundsListPage.tsx";
+import { ROUTES } from "./routes.ts";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/*TODO: вынести пути*/}
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/rounds" element={<RoundListPage />} />
-        <Route path="/round/:id" element={<RoundPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path={ROUTES.LOGIN.path} element={<LoginPage />} />
+        <Route path={ROUTES.ROUNDS.path} element={<RoundListPage />} />
+        <Route path={ROUTES.ROUND.path} element={<RoundPage />} />
+        <Route path={ROUTES.NOT_FOUND.path} element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+export const ROUTES = {
+  LOGIN: { path: "/login" },
+  ROUNDS: { path: "/rounds" },
+  ROUND: { path: "/round/:id" },
+  NOT_FOUND: { path: "*" },
+};
